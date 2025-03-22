1e2ee4281586fe73996deb7bc945c24afcbc4950
orenlugashi <orenlugashi@gmail.com>
2025-03-22 17:53:11 +0200

    add flag if register to the event

--- src/components/CameraComponent.tsx
+++ src/components/CameraComponent.tsx
@@ -4,19 +4,24 @@ import { CameraData } from '../models/CameraModel';
 
 const CameraComponent: React.FC = () => {
   const [cameraData, setCameraData] = useState<CameraData>({ cameraX: 0, cameraY: 0, cameraZ: 0 });
+  const [isReceivingData, setIsReceivingData] = useState(false);
 
   useEffect(() => {
-    const handleCameraData = (data: CameraData) => {
+    const handleCameraData = async (data: CameraData) => {
       setCameraData(data);
       sendDataToServer(data);
     };
 
-    eventBus.on('cameraData', handleCameraData);
+    if (isReceivingData) {
+      eventBus.on('cameraData', handleCameraData);
+    } else {
+      eventBus.off('cameraData', handleCameraData);
+    }
 
     return () => {
       eventBus.off('cameraData', handleCameraData);
     };
-  }, []);
+  }, [isReceivingData]);
 
   const sendDataToServer = (data: CameraData) => {
     console.log('Camera data:', data);
@@ -25,6 +30,9 @@ const CameraComponent: React.FC = () => {
   return (
     <div>
       <h2>Camera Controls</h2>
+      <button onClick={() => setIsReceivingData(!isReceivingData)}>
+        {isReceivingData ? 'Disable' : 'Enable'} Receiving Data
+      </button>
       <div>Camera X: {cameraData.cameraX.toFixed(2)}</div>
       <div>Camera Y: {cameraData.cameraY.toFixed(2)}</div>
       <div>Camera Z: {cameraData.cameraZ.toFixed(2)}</div>
--- src/components/DrivingComponent.tsx
+++ src/components/DrivingComponent.tsx
@@ -4,19 +4,24 @@ import { DrivingData } from '../models/DrivingModel';
 
 const DrivingComponent: React.FC = () => {
   const [drivingData, setDrivingData] = useState<DrivingData>({ steer: 0, gas: 0, brake: 0 });
+  const [isReceivingData, setIsReceivingData] = useState(false);
 
   useEffect(() => {
-    const handleDrivingData = (data: DrivingData) => {
+    const handleDrivingData = async (data: DrivingData) => {
       setDrivingData(data);
       sendDataToServer(data);
     };
 
-    eventBus.on('drivingData', handleDrivingData);
+    if (isReceivingData) {
+      eventBus.on('drivingData', handleDrivingData);
+    } else {
+      eventBus.off('drivingData', handleDrivingData);
+    }
 
     return () => {
       eventBus.off('drivingData', handleDrivingData);
     };
-  }, []);
+  }, [isReceivingData]);
 
   const sendDataToServer = (data: DrivingData) => {
     console.log('Driving data:', data);
@@ -25,6 +30,9 @@ const DrivingComponent: React.FC = () => {
   return (
     <div>
       <h2>Driving Controls</h2>
+      <button onClick={() => setIsReceivingData(!isReceivingData)}>
+        {isReceivingData ? 'Disable' : 'Enable'} Receiving Data
+      </button>
       <div>Steer: {drivingData.steer.toFixed(2)}</div>
       <div>Gas: {drivingData.gas.toFixed(2)}</div>
       <div>Brake: {drivingData.brake.toFixed(2)}</div>
